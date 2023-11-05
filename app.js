6ab6f28971bdac046e2835bf9bc14292b67c373d
JacksonYang0315 <jacksonyang0315@gmail.com>
2023-11-05 06:12:16 +0000

    push

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get('/specific', (req, res) => {
     res.sendFile(path.join(__dirname, '/page2.html'));
 })
 
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, '/p3test2.html'));
+})
+
 
 app.get('/indextest4', (req, res) => {
     res.sendFile(path.join(__dirname, '/indextest4.html'));
